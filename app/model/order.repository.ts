73acda4c175a0243190c3b8e93e579fff3444be9
lihuazhidao <765794900@qq.com>
2017-02-22 18:25:30 +0800

    ..add

--- app/model/order.repository.ts
+++ app/model/order.repository.ts
@@ -4,3 +4,17 @@ import { Order } from "./order.model";
 import { StaticDataSource } from "./static.datasource";
 
 
+
+@Injectable()
+export class OrderRepository {
+  private orders: Order[] = [];
+  constructor(private dataSource: StaticDataSource) {}
+  getOrders(): Order[] {
+    return this.orders;
+  }
+
+  saveOrder(order: Order): Observable<Order> {
+    return this.dataSource.saveOrder(order);
+  }
+
+}
